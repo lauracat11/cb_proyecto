c1b1e252b34e9207cb7d13473565349560b30f3d
lauracat11 <92210196+lauracat11@users.noreply.github.com>
2022-11-30 16:15:30 +0100

    update para que funcione

--- php/registro.php
+++ php/registro.php
@@ -29,7 +29,7 @@ if (mysqli_num_rows($res)>0){
     $sql="INSERT INTO users (ID, nombre, apellidos, email, pswd) VALUES ('$id', '$uname', '$auname', '$uemail', '$pswd')";
 
     if (mysqli_query($conn, $sql)){
-        header("Location: ../index.html");
+        header("Location: /cb_proyecto/index.html");
     }else{
         echo "Fallo al ingreasar el registro en: " . $sql . mysqli_error($conn);
     }
