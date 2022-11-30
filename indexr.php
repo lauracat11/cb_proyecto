deae7f784a6b8196f52e50532d57717970d980f5
ArnauBrull <91909683+ArnauBrull@users.noreply.github.com>
2022-11-30 15:36:12 +0100

    CSS RESURRECTED

--- indexr.php
+++ indexr.php
@@ -8,14 +8,8 @@
     <link rel="stylesheet" href="css/registro.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
     <link rel="icon" type="image/x-icon" href="imgs/favicon/favicon-96x96.png">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
-
 </head>
-
 <body>
-
     <div class="container">
         <div class="wrapper">
             <div class="title"><svg width="880px" height="190px" viewBox="-46 0 310 45" version="1.1"
@@ -100,8 +94,5 @@
             </form>
         </div>
     </div>
-
-
 </body>
-
 </html>
